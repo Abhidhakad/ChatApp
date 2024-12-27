b08d217ebce933080d675d6aa2c65c26da758881
Abhishek Nagar <nabhishek734@gmail.com>
2024-12-27 15:53:26 +0530

    feature: added userSchema

--- backend/src/models/messageModel.js
+++ backend/src/models/messageModel.js
@@ -1,25 +1,5 @@
 import mongoose from "mongoose";
 
-// const messageSchema = new mongoose.Schema(
-//   {
-//     conversationId: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "Conversation",
-//       required: true,
-//     },
-//     sender: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",
-//       required: true,
-//     },
-//     text: {
-//       type: String,
-//       required: true,
-//     },
-//   },
-//   { timestamps: true }
-// );
-
 const messageSchema = new mongoose.Schema({
   sender: {
     type: mongoose.Schema.Types.ObjectId,
---
+++ backend/src/models/userModel.js
@@ -0,0 +1,81 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      maxlength: [50, "Name cannot exceed 50 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate: {
+        validator: function (email) {
+          return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+        },
+        message: "Please provide a valid email address",
+      },
+    },
+    bio: {
+      type: String,
+      default: "Available",
+      maxlength: [160, "Bio cannot exceed 160 characters"],
+    },
+    profilePic: {
+      type: String,
+      default:
+        "https://www.shutterstock.com/image-vector/user-profile-icon-vector-avatar-600nw-2247726673.jpg",
+    },
+    contacts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters"],
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw new Error("Error while comparing passwords");
+  }
+};
+
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign(
+      { id: this._id, email: this.email },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "24h",
+      }
+    );
+
+    return token;
+  } catch (error) {
+    console.log("error while generating token");
+  }
+};
+export default mongoose.model("User", userSchema);
